1d65793adff062489aed344bf5aceca054259fac
Zen6ka <131902026+Zen6ka@users.noreply.github.com>
2023-11-23 09:23:09 +0000

    Deploying to gh-pages from @ Zen6ka/BrainAttack-js-proj@ecdc9859638e24826b61ad80f76219d67b347451 🚀

---
+++ product-markup.js
@@ -0,0 +1,13 @@
+// let markup = `<li class="cardlist-item id-for-del" data-id=${id}>
+//         <img src="${product}" loading="lazy" class="cardlist-img" alt="${procust}" onerror="this.onerror=null;this.src='${picUrl}';" width=300>
+//         <h3 class="cardlist-product">${product}</h3>
+//         <p class="cardlist-descr">${description} </p>
+//         <div class="cartlist-btn"><button class="cardlist-add-cart add-to-cart-product ${classAd}">
+//         <svg class="cardlist-svg" weight="18" height="18">
+//         <use href="${spriteURL}#cart"></use>
+//         </svg>
+//         </button>
+//         </div>
+//         </li>`;
+//   return markup;
+// }
