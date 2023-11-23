3c1ee9d23fd9c9c28db0cd2e955265a83aa5b9f6
o-kovalenko01 <kovalenkoleg777@gmail.com>
2023-11-23 23:20:06 +0200

    create section

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,27 +1,24 @@
-const cartItemsQuantitu = document.querySelector(".js-cart-items-quantity"); // Місце де буде оновлюватись кількість товарів в кошику
-const cartEmpty = document.querySelector(".js-cart-empty");
-const cartContainer = document.querySelector(".js-cart-container");
+const cartItemsQuantitu = document.querySelector('.js-cart-items-quantity'); // Місце де буде оновлюватись кількість товарів в кошику
+const cartEmpty = document.querySelector('.js-cart-empty');
+const cartContainer = document.querySelector('.js-cart-container');
 const deleteAllBtn = document.querySelector('.js-delete-all-btn');
-const cartSelectedProducts = document.querySelector(".js-cart-selected-products");
-const cartOrderDetails = document.querySelector(".js-cart-order-details");
-const totalOrderedPrice = document.querySelector(".js-total-ordered-price");
-const form = document.querySelector(".js-email-form");
-const btnCheckout = document.querySelector(".js-email-checkout");
+const cartSelectedProducts = document.querySelector(
+  '.js-cart-selected-products'
+);
+const cartOrderDetails = document.querySelector('.js-cart-order-details');
+const totalOrderedPrice = document.querySelector('.js-total-ordered-price');
+const form = document.querySelector('.js-email-form');
+const btnCheckout = document.querySelector('.js-email-checkout');
 
-
-
-
-
-console.log("sdsd");
+// console.log("sdsd");
 // За замовчуванням сторінка кошика буде пустою.
 // cartEmptyHidden();
 // cartContainerHidden();
 
-
-
 // ФУНКЦІЯ ПЕРЕВІРКИ ЛОКАЛЬНОГО СХОВИЩА НА ВМІСТ ДАНИХ
-function localStorageCheck() { // ****** ізніше підшаманити, щоб просто повертати результат сховище, а логіку відпрацьовувати далі поза функцією.
-  const savedProducts = localStorage.getItem("cart-products");
+function localStorageCheck() {
+  // ****** ізніше підшаманити, щоб просто повертати результат сховище, а логіку відпрацьовувати далі поза функцією.
+  const savedProducts = localStorage.getItem('cart-products');
   const parsedSavedProducts = JSON.parse(savedProducts);
 
   cartItemsQuantitu.innerHTML = parsedSavedProducts.length;
@@ -29,20 +26,19 @@ function localStorageCheck() { // ****** ізніше підшаманити, щ
   const { name, img, category, size, price } = parsedSavedProducts; // повинен повертатись об`єкт за запитом до сховища. Тому одразу роблю його деструктуризацію, щоб потім відмальовувати розмітку.
 
   // метод редюс буде рахувати мені суму цін за всі продукти. Потім цей результат я буду передавати в Тотал під час перевірки.
-//   const sumPrice = parsedSavedProducts.reduce((acc, currentProduct)=>{
-// return acc + currentPrice.price;
-//   }  , 0) 
-
+  //   const sumPrice = parsedSavedProducts.reduce((acc, currentProduct)=>{
+  // return acc + currentPrice.price;
+  //   }  , 0)
 
   if (savedProducts.length === 0) {
     // або пустий масив/об`єкт null/undefined (потім подивитись що повертається і підкорегувати логіку)
     // ТОДІ виклик функції яка покаже нашу порожню розмітку і приховає розмітку товарів.
-// cartEmptyShow();
-// cartContainerHidden();
+    // cartEmptyShow();
+    // cartContainerHidden();
   } else {
     // Якщо масив не порожній, тоді приховуємо розмітку порожнього масиву і показужмо розмітку товарів.
-//     cartEmptyHidden();
-// cartContainerShow();
+    //     cartEmptyHidden();
+    // cartContainerShow();
 
     //     // виклик функції з відмалюванням даних продукту i передача її результату в дівчик
     //     const productMarkup = cartSelectedProducts();
@@ -52,30 +48,25 @@ function localStorageCheck() { // ****** ізніше підшаманити, щ
     // const detailsMarkup = orderDetailsMarkup ();
     // cartOrderDetails.innerHTML = detailsMarkup;
 
-
-// передаємо суму цін в Тотал
+    // передаємо суму цін в Тотал
     // totalOrderedPrice.innerHTML = `$${sumPrice}`;
 
     // Слухач на форму по сабміту а ще треба буде по кліку на кнопку
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-})
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+    });
   }
 }
 
-
-
-deleteAllBtn.addEventListener('click', removeLocalStorage);
+// deleteAllBtn.addEventListener('click', removeLocalStorage);
 // ФУНКЦІЯ ОЧИЩЕННЯ СХОВИЩА ВІД ВСІХ ПРОДУКТІВ
-function removeLocalStorage(event){
-    event.preventDefault();
-    localStorage.removeItem('cart-products'); // очищую сховище
-    cartSelectedProducts.innerHTML = "";
-    cartContainerHidden(); // приховую контейнер кошика
-                           // показую пустий кошик
-
+function removeLocalStorage(event) {
+  event.preventDefault();
+  localStorage.removeItem('cart-products'); // очищую сховище
+  cartSelectedProducts.innerHTML = '';
+  cartContainerHidden(); // приховую контейнер кошика
+  // показую пустий кошик
 }
-
 
 // ФУНКЦІЯ ВІДМАЛЬОВУВАННЯ ОБРАНИХ ТОВАРІВ
 function selectedProductsMarkup(
@@ -95,7 +86,7 @@ function selectedProductsMarkup(
         <h3>$${productPrice}</h3>
     </div>
 </div>
-`
+`;
 }
 
 // На разі закую її просто в розмітку і буду приховувати або показувати в залежності від результату
@@ -115,25 +106,23 @@ function selectedProductsMarkup(
 // функції помічники які будуть викликатись навзаєм в залежності від того чи в локальному сховищі щось є
 
 // ФУНКЦІЯ ПОКАЗУ ПОРОЖНЬОГО КОШИКА - EMPTY
-function cartEmptyShow () {
-    cartEmpty.style.display = 'block';
+function cartEmptyShow() {
+  cartEmpty.style.display = 'block';
 }
 // ФУНКЦІЯ ПРИХОВУВАННЯ ПОРОЖНЬОГО КОШИКА - EMPTY
-function cartEmptyHidden () {
-    cartEmpty.style.display = 'none';
+function cartEmptyHidden() {
+  cartEmpty.style.display = 'none';
 }
 
-
 // ФУНКЦІЯ ПОКАЗУ ПОРОЖНЬОГО КОНТЕЙНЕРУ
-function cartContainerShow () {
-    cartContainer.style.display = 'block';
+function cartContainerShow() {
+  cartContainer.style.display = 'block';
 }
 // ФУНКЦІЯ Приховування ПОРОЖНЬОГО КОНТЕЙНЕРУ
 // --------- буду викликати функцію яка приховує вміст коли отриманий результат запиту буде не порожнім
-function cartContainerHidden () {
-    cartContainer.style.display = 'none';
+function cartContainerHidden() {
+  cartContainer.style.display = 'none';
 }
-
 
 // НА РАЗІ ЦЕ НЕ ПОТРІБНО БО МИ БУДЕМО ПРИХОВУВАТИ УВЕСЬ КОНТЕЙНЕР
 // // ФУНКЦІЯ ПРИХОВУВАННЯ ДЕТАЛЕЙ ЗАМОВЛЕННЯ
@@ -145,4 +134,4 @@ function cartContainerHidden () {
 //   cartOrderDetails.style.display = "block";
 // }
 
-console.log("qwewqe");
+// console.log('qwewqe');
--- src/js/filters.js
+++ src/js/filters.js
@@ -1,32 +1,31 @@
 import axios from 'axios';
 
-
 export class RequestToTheServer {
-    baseUrl = 'https://food-boutique.b.goit.study/api/'
+  baseUrl = 'https://food-boutique.b.goit.study/api/';
 
-    constructor(endPoint){
-        this.endPoint = endPoint;
-    }
+  constructor(endPoint) {
+    this.endPoint = endPoint;
+  }
 
-    async fetchBreeds(){
-    try{
-        const response = await axios.get(`${this.baseUrl}${this.endPoint}`);
-        console.log(response.data);
-        return response.data
-    } catch(error){
-        console.error("Error:", error.message);
+  async fetchBreeds() {
+    try {
+      const response = await axios.get(`${this.baseUrl}${this.endPoint}`);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error:', error.message);
     }
-    }
-};
+  }
+}
 
 const searchForm = document.querySelector('.search-form');
 const inputSearch = document.querySelector('.input-search');
 const buttonSearch = document.querySelector('.button-search');
 const filtersResult = document.querySelector('.filters-result');
-let productsHomePage ={};
+let productsHomePage = {};
 
 const messageForError = () => {
-    const htmlError = `<div class="error">
+  const htmlError = `<div class="error">
     <p class="title-error">
         Nothing was found for the selected <span><a class="a-title-error" href="">filters...</a></span>
     </p>
@@ -34,54 +33,47 @@ const messageForError = () => {
         Try adjusting your search parameters or browse our range by other criteria to find the perfect product for you.
     </p>
 </div>`;
-    filtersResult.innerHTML = htmlError;
-}
+  filtersResult.innerHTML = htmlError;
+};
 
 async function ifEmptyInput() {
-    try {
-        const storageDataHomePage = localStorage.getItem('products-home-page-filters');
-        if (storageDataHomePage) {
-            productsHomePage = JSON.parse(storageDataHomePage);
-        } else {
-            const firstProductsFilters = "products";
-            const classFirstProducts = new RequestToTheServer(firstProductsFilters);
-            productsHomePage = await classFirstProducts.fetchBreeds();
-            localStorage.setItem('products-home-page-filters', JSON.stringify(productsHomePage));
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
+  try {
+    const storageDataHomePage = localStorage.getItem(
+      'products-home-page-filters'
+    );
+    if (storageDataHomePage) {
+      productsHomePage = JSON.parse(storageDataHomePage);
+    } else {
+      const firstProductsFilters = 'products';
+      const classFirstProducts = new RequestToTheServer(firstProductsFilters);
+      productsHomePage = await classFirstProducts.fetchBreeds();
+      localStorage.setItem(
+        'products-home-page-filters',
+        JSON.stringify(productsHomePage)
+      );
     }
-    console.log(productsHomePage);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+  // console.log(productsHomePage);
 }
 
 ifEmptyInput();
 
-
-
-
-
-
-
-
-
-
-searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const textInputFilters = inputSearch.value.trim();
-    if(!textInputFilters){
-        ifEmptyInput();
-        filtersResult.innerHTML = '';
-    } else{
-        const valueFilters = `products?keyword=${textInputFilters}`;
-            const classResultProductsWithFilters = new RequestToTheServer(valueFilters);
-            productsHomePage = await classResultProductsWithFilters.fetchBreeds();
-            // localStorage.setItem('products-with-filters', JSON.stringify(productsHomePage));
-            if(productsHomePage.totalPages === 0){
-                messageForError();
-            }
+searchForm.addEventListener('submit', async event => {
+  event.preventDefault();
+  const textInputFilters = inputSearch.value.trim();
+  if (!textInputFilters) {
+    ifEmptyInput();
+    filtersResult.innerHTML = '';
+  } else {
+    const valueFilters = `products?keyword=${textInputFilters}`;
+    const classResultProductsWithFilters = new RequestToTheServer(valueFilters);
+    productsHomePage = await classResultProductsWithFilters.fetchBreeds();
+    // localStorage.setItem('products-with-filters', JSON.stringify(productsHomePage));
+    if (productsHomePage.totalPages === 0) {
+      messageForError();
     }
-    console.log(productsHomePage.totalPages);
-})
-
-
-
+  }
+  console.log(productsHomePage.totalPages);
+});
