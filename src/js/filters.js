1d20c72396aec2aa13467244766d5ecec06b8ffb
NatHumeniuk <nat.gumenuk@gmail.com>
2023-11-24 23:55:38 +0200

    modal policy

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,73 +1,82 @@
 import axios, { all } from 'axios';
 
 export class RequestToTheServer {
-    baseUrl = 'https://food-boutique.b.goit.study/api/'
+  baseUrl = 'https://food-boutique.b.goit.study/api/';
 
-    constructor(endPoint, filters, page, limit){
-        this.endPoint = endPoint;
-        this.filters = filters;
-        this.page = page;
-        this.limit = limit;
-    }
+  constructor(endPoint, filters, page, limit) {
+    this.endPoint = endPoint;
+    this.filters = filters;
+    this.page = page;
+    this.limit = limit;
+  }
 
-    async fetchBreeds(){
-    try{
-        const response = await axios.get(`${this.baseUrl}${this.endPoint}?${this.filters}&page=${this.page}&limit=${this.limit}`);
-        console.log(response.data);
-        return response.data
-    } catch(error){
-        console.error("Error:", error.message);
+  async fetchBreeds() {
+    try {
+      const response = await axios.get(
+        `${this.baseUrl}${this.endPoint}?${this.filters}&page=${this.page}&limit=${this.limit}`
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error:', error.message);
     }
-    }
-};
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const searchForm = document.querySelector('.search-form');
 const inputSearch = document.querySelector('.first-input-search');
 const filtersResult = document.querySelector('.filters-result');
-const firctSelectSearch = document.querySelector('.first-select-search-not-focus');
+const firctSelectSearch = document.querySelector(
+  '.first-select-search-not-focus'
+);
 const buttonCategories = document.querySelector('.button-categories');
 const spanButtonCategories = document.querySelector('.span-button-categories');
 const cardList = document.querySelector('.card-list');
 
-
-const products = "products";
+const products = 'products';
 
 let keyword = '';
 let category = '';
 let page = 1;
 let limit = 6;
-let productsHomePage ={};
+let productsHomePage = {};
 let inputResultSearch = {};
 let productsCategories = {};
-let fullInputResultSearch ={};
+let fullInputResultSearch = {};
 
-function recordsDataForSearch(keyword, category, page, limit){
-    localStorage.setItem('data-for-search', JSON.stringify(
-        {
-            keyword, 
-            category,
-            page,
-            limit
-        }
-        ))
-};
+function recordsDataForSearch(keyword, category, page, limit) {
+  localStorage.setItem(
+    'data-for-search',
+    JSON.stringify({
+      keyword,
+      category,
+      page,
+      limit,
+    })
+  );
+}
 
 recordsDataForSearch(keyword, category, page, limit);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-async function search () {
-    const letForSearch = JSON.parse(localStorage.getItem('data-for-search'));
-    const filters = `keyword=${letForSearch.keyword}&category=${letForSearch.category}`;
-        const classResultProductsWithFilters = new RequestToTheServer(products, filters, page, limit);
-        fullInputResultSearch = await classResultProductsWithFilters.fetchBreeds();
-};
+async function search() {
+  const letForSearch = JSON.parse(localStorage.getItem('data-for-search'));
+  const filters = `keyword=${letForSearch.keyword}&category=${letForSearch.category}`;
+  const classResultProductsWithFilters = new RequestToTheServer(
+    products,
+    filters,
+    page,
+    limit
+  );
+  fullInputResultSearch = await classResultProductsWithFilters.fetchBreeds();
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const messageForError = () => {
-    const htmlError = `<div class="error">
+  const htmlError = `<div class="error">
     <p class="title-error">
         Nothing was found for the selected <span><a class="a-title-error" href="">filters...</a></span>
     </p>
@@ -75,118 +84,140 @@ const messageForError = () => {
         Try adjusting your search parameters or browse our range by other criteria to find the perfect product for you.
     </p>
 </div>`;
-    filtersResult.innerHTML = htmlError;
-}
+  filtersResult.innerHTML = htmlError;
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////// HOME /// PAGE ////////////////////////////////////////////////////////////
 async function ifEmptyInput() {
-    try {
-        const storageDataHomePage = localStorage.getItem('products-home-page-filters');
-        if (storageDataHomePage) {
-            productsHomePage = JSON.parse(storageDataHomePage);
-            // localStorage.removeItem('products-home-page-filters');
-        } else {
-            await search();
-            productsHomePage = fullInputResultSearch.results;
-            console.log(fullInputResultSearch);
-            localStorage.setItem('products-home-page-filters', JSON.stringify(productsHomePage));
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
+  try {
+    const storageDataHomePage = localStorage.getItem(
+      'products-home-page-filters'
+    );
+    if (storageDataHomePage) {
+      productsHomePage = JSON.parse(storageDataHomePage);
+      // localStorage.removeItem('products-home-page-filters');
+    } else {
+      await search();
+      productsHomePage = fullInputResultSearch.results;
+      console.log(fullInputResultSearch);
+      localStorage.setItem(
+        'products-home-page-filters',
+        JSON.stringify(productsHomePage)
+      );
     }
-    console.log(productsHomePage);
-    renderCards(productsHomePage)
-    // localStorage.removeItem('products-home-page-filters');
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+  console.log(productsHomePage);
+  renderCards(productsHomePage);
+  // localStorage.removeItem('products-home-page-filters');
 }
 
 ifEmptyInput();
 
 //////////////////////////////////////// INPUT ////////////////////////////////////////////////////////////////////
 
-searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    keyword = inputSearch.value.trim();
-    recordsDataForSearch(keyword, category, page, limit);
-    await search();
-    inputResultSearch = fullInputResultSearch.results;
-    console.log(inputResultSearch);
-    if(fullInputResultSearch.totalPages === 0){
-        messageForError();
-    }
+searchForm.addEventListener('submit', async event => {
+  event.preventDefault();
+  keyword = inputSearch.value.trim();
+  recordsDataForSearch(keyword, category, page, limit);
+  await search();
+  inputResultSearch = fullInputResultSearch.results;
+  console.log(inputResultSearch);
+  if (fullInputResultSearch.totalPages === 0) {
+    messageForError();
+  }
 });
 
 ////////////////////////////////////////// ALL CATEGORIES ////////////////////////////////////////////////////////////////////
 
 async function ifEmptyCategories() {
-    try {
-        const storageDataCategories = localStorage.getItem('categories-filters');
-        if (storageDataCategories) {
-            productsCategories = JSON.parse(storageDataCategories);
-            // localStorage.removeItem('categories-filters');
-        } else {
-            const filters = '';
-            const firstProductsCategoriesFilters = `${products}/categories`;
-            const classFirstCategoriesProducts = new RequestToTheServer(firstProductsCategoriesFilters, filters, page, limit);
-            productsCategories = await classFirstCategoriesProducts.fetchBreeds();
-            localStorage.setItem('categories-filters', JSON.stringify(productsCategories));
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
+  try {
+    const storageDataCategories = localStorage.getItem('categories-filters');
+    if (storageDataCategories) {
+      productsCategories = JSON.parse(storageDataCategories);
+      // localStorage.removeItem('categories-filters');
+    } else {
+      const filters = '';
+      const firstProductsCategoriesFilters = `${products}/categories`;
+      const classFirstCategoriesProducts = new RequestToTheServer(
+        firstProductsCategoriesFilters,
+        filters,
+        page,
+        limit
+      );
+      productsCategories = await classFirstCategoriesProducts.fetchBreeds();
+      localStorage.setItem(
+        'categories-filters',
+        JSON.stringify(productsCategories)
+      );
     }
-    renderCategories(productsCategories);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+  renderCategories(productsCategories);
 }
 
 ifEmptyCategories();
 
-function renderCategories(productsCategories){
-    const listCategories = [];
-    productsCategories.forEach((productsCategorie) => {
-        const itemCategories = `<li class="li-first-select-search"><button class="button-li-filters">${productsCategorie.replace(/_/g, ' ').replace(/&/g, '/')}</button></li>`;
-        listCategories.push(itemCategories)
-    });
-    firctSelectSearch.insertAdjacentHTML('beforeend', listCategories.join(''));
-    const buttonsLiFilters = document.querySelectorAll('.button-li-filters');
-    addListenerLi(buttonsLiFilters)
+function renderCategories(productsCategories) {
+  const listCategories = [];
+  productsCategories.forEach(productsCategorie => {
+    const itemCategories = `<li class="li-first-select-search"><button class="button-li-filters">${productsCategorie
+      .replace(/_/g, ' ')
+      .replace(/&/g, '/')}</button></li>`;
+    listCategories.push(itemCategories);
+  });
+  firctSelectSearch.insertAdjacentHTML('beforeend', listCategories.join(''));
+  const buttonsLiFilters = document.querySelectorAll('.button-li-filters');
+  addListenerLi(buttonsLiFilters);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-buttonCategories.addEventListener('click', () => addListenerButton(buttonCategories, firctSelectSearch));
+buttonCategories.addEventListener('click', () =>
+  addListenerButton(buttonCategories, firctSelectSearch)
+);
 
 function addListenerButton(button, buttonList) {
-    buttonList.classList.add('first-select-search');
-    document.addEventListener('click', (event) => workButtonMenu(event, button, buttonList));
-};
+  buttonList.classList.add('first-select-search');
+  document.addEventListener('click', event =>
+    workButtonMenu(event, button, buttonList)
+  );
+}
 
 function workButtonMenu(event, button, listButtonMenu) {
-    if(!button.contains(event.target)&&!listButtonMenu.contains(event.target)){
-        listButtonMenu.classList.remove('first-select-search')
-    } else if(listButtonMenu.contains(event.target)){
-        setTimeout(() => {
-            listButtonMenu.classList.remove('first-select-search')
-        }, 100)
-    }
-};
+  if (
+    !button.contains(event.target) &&
+    !listButtonMenu.contains(event.target)
+  ) {
+    listButtonMenu.classList.remove('first-select-search');
+  } else if (listButtonMenu.contains(event.target)) {
+    setTimeout(() => {
+      listButtonMenu.classList.remove('first-select-search');
+    }, 100);
+  }
+}
 
-function addListenerLi(buttonsLiFilters){
-    buttonsLiFilters.forEach((buttonLiFilters) => {
-    buttonLiFilters.addEventListener('click', renderEndPoint)
-})
-};
+function addListenerLi(buttonsLiFilters) {
+  buttonsLiFilters.forEach(buttonLiFilters => {
+    buttonLiFilters.addEventListener('click', renderEndPoint);
+  });
+}
 
-function renderEndPoint(event){
-    const nameCategoryForSelect = event.currentTarget.textContent;
-    category = nameCategoryForSelect.replace(/ /g, '_').replace(/\//g, '&');
-    spanButtonCategories.innerHTML = `${nameCategoryForSelect}`;
+function renderEndPoint(event) {
+  const nameCategoryForSelect = event.currentTarget.textContent;
+  category = nameCategoryForSelect.replace(/ /g, '_').replace(/\//g, '&');
+  spanButtonCategories.innerHTML = `${nameCategoryForSelect}`;
 }
 
 ///////////////////////////////////////////////////////  RENDER  CARDS  /////////////////////////////////////////////////////////////
 
 function renderCards(products) {
-    const listResult = [];
-    products.forEach((product) => {
-        const itemResult = `<li class="card-list-item id-for-del" data-id=${product._id}>
+  const listResult = [];
+  products.forEach(product => {
+    const itemResult = `<li class="card-list-item id-for-del" data-id=${product._id}>
                 <img src="${product.img}" loading="lazy" class="cardlist-img" alt="${product.name}" onerror="this.onerror=null;this.src='';" width=300>
                 <h3 class="card-list-product">${product.name}</h3>
                 <ul class="cardlist-descr">
@@ -201,66 +232,47 @@ function renderCards(products) {
                 </button>
                 </div>
                 </li>`;
-                listResult.push(itemResult)
-    });
-    cardList.innerHTML = listResult.join(" ")
-};
-
+    listResult.push(itemResult);
+  });
+  cardList.innerHTML = listResult.join(' ');
+}
 
 // localStorage.clear()
-
-
-
-
-
-
-
-
 
 // const allValueInputLS = localStorage.getItem('all-value-input');
 
 // async function functionInputSearch(textInputFilters, allValueInputLS, nameCategory){
-    // let resultSearch = localStorage.getItem('result-search-filters');
-    // if(!textInputFilters && nameCategory === ''){
-    //     ifEmptyInput();
-    //     filtersResult.innerHTML = '';
-    // } else if(allValueInputLS){
-    //         const allValueInput = JSON.parse(allValueInputLS);
-    //         allValueInput.push(textInputFilters)
-    //         const uniqueAllValueInput = allValueInput.filter(
-    //             (value, index, array) => array.indexOf(value) === index
-    //         );
-    //         localStorage.setItem('all-value-input', JSON.stringify(uniqueAllValueInput));
-    //         // localStorage.removeItem('all-value-input');
-    //         console.log(uniqueAllValueInput);
-    //         if(allValueInput.find(value => value === textInputFilters) && resultSearch){
-    //             const massOldResult = JSON.parse(resultSearch);
-    //             inputResultSearch = massOldResult.filter(
-    //                 obj => obj.name.toLowerCase().includes(textInputFilters.toLowerCase())
-    //                 ).filter(obj => obj.category === nameCategory);
-    //             if(Object.keys(inputResultSearch).length === 0){
-    //                 await searchWithFilters(resultSearch, textInputFilters, nameCategory)
-    //             }
-    //             console.log(inputResultSearch);
-    //         } else {
-    //             searchWithFilters(resultSearch, textInputFilters, nameCategory)
-    //         }
-    //     } else {
-    //         console.log([textInputFilters]);
-    //         localStorage.setItem('all-value-input', JSON.stringify([textInputFilters]));
-    //         searchWithFilters(resultSearch, textInputFilters, nameCategory)
-    // }
-    // };
-
-
-
-
-
-
-
-
-
-
+// let resultSearch = localStorage.getItem('result-search-filters');
+// if(!textInputFilters && nameCategory === ''){
+//     ifEmptyInput();
+//     filtersResult.innerHTML = '';
+// } else if(allValueInputLS){
+//         const allValueInput = JSON.parse(allValueInputLS);
+//         allValueInput.push(textInputFilters)
+//         const uniqueAllValueInput = allValueInput.filter(
+//             (value, index, array) => array.indexOf(value) === index
+//         );
+//         localStorage.setItem('all-value-input', JSON.stringify(uniqueAllValueInput));
+//         // localStorage.removeItem('all-value-input');
+//         console.log(uniqueAllValueInput);
+//         if(allValueInput.find(value => value === textInputFilters) && resultSearch){
+//             const massOldResult = JSON.parse(resultSearch);
+//             inputResultSearch = massOldResult.filter(
+//                 obj => obj.name.toLowerCase().includes(textInputFilters.toLowerCase())
+//                 ).filter(obj => obj.category === nameCategory);
+//             if(Object.keys(inputResultSearch).length === 0){
+//                 await searchWithFilters(resultSearch, textInputFilters, nameCategory)
+//             }
+//             console.log(inputResultSearch);
+//         } else {
+//             searchWithFilters(resultSearch, textInputFilters, nameCategory)
+//         }
+//     } else {
+//         console.log([textInputFilters]);
+//         localStorage.setItem('all-value-input', JSON.stringify([textInputFilters]));
+//         searchWithFilters(resultSearch, textInputFilters, nameCategory)
+// }
+// };
 
 // const productsFromTheLS = JSON.parse(localStorage.getItem('products-home-page-filters')).results;
 // resultSearch = localStorage.getItem('result-search-filters');
@@ -271,15 +283,7 @@ function renderCards(products) {
 //                 }
 //             });
 
-
-
-
-
-
 // async function searchWithFilters(textInputFilters, nameCategory) {
-    
-
-
 
 //         if(fullInputResultSearch.totalPages === 0){
 //             messageForError();
--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,14 @@
-//==============open and close modal policy and terms========
+import axios from 'axios';
+
+import { RequestToTheServer } from './filters';
 
 const refs = {
+  // ---------------for form subscription--------------
+
+  formSubscription: document.querySelector('.contact-form-input'),
+
+  // ----------------for modal window_____________
+
   openModalPolicy: document.querySelector('.js-modal-policy-open'),
   openModalTerms: document.querySelector('.js-modal-terms-open'),
   closeModalPolicyBtn: document.querySelector('.js-policy-close'),
@@ -8,6 +16,18 @@ const refs = {
   policyLink: document.querySelector('.js-policy'),
   termsLink: document.querySelector('.js-terms'),
 };
+
+//==============form subscription========
+
+refs.formSubscription.addEventListener('submit', onFormSubscriptSubmit);
+
+function onFormSubscriptSubmit(e) {
+  e.preventDefault();
+  const email = e.currentTarget.elements.email.value;
+  console.log(email);
+}
+
+//==============open and close modal policy and terms========
 
 refs.openModalPolicy.addEventListener('click', () => {
   onPolicyClick();
@@ -25,3 +45,5 @@ function onPolicyClick() {
 function onTermsClick() {
   refs.termsLink.classList.toggle('is-hidden-policy');
 }
+document.body.classList.add('.no-scroll');
+document.body.classList.remove('.no-scroll');
